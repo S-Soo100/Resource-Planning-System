27aad48dec4d73a953d9727d9f08a5197755604d
Seungsoo Baek <bss.rol20@gmail.com>
2025-01-20 14:11:34 +0900

    app bar 컴포넌트 ui

--- src/components/appbar/appbar.tsx
+++ src/components/appbar/appbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
-
-import { FaBox } from "react-icons/fa";
+import { User } from "lucide-react";
 import { IoLogOutOutline } from "react-icons/io5";
 import Cookies from "js-cookie";
 import { useRouter } from "next/navigation";
@@ -12,15 +11,16 @@ export default function AppBar() {
     router.push("/signin");
   };
   return (
-    <div className="flex items-center justify-between bg-blue-500 text-white p-4">
+    <div className="flex items-center justify-between bg-gray-800 text-white p-4">
       <div className="flex items-center space-x-2">
         <div className="bg-gray-200 rounded-full p-2">
           {/* User Icon */}
-          <FaBox className="text-blue-500 text-2xl" />
+          {/* <FaBox className="text-blue-500 text-2xl" /> */}
+          <User className="h-6 w-6 text-blue-600" />
         </div>
-        <span className="font-medium">User</span>
+        <span className="font-medium">자동차왕 헨리 초이</span>
       </div>
-      <div className="bg-gray-800 rounded-full p-2">
+      <div className="bg-slate-700 rounded-full p-2">
         {/* User Icon */}{" "}
         <IoLogOutOutline className="text-white text-2xl" onClick={logout} />
       </div>

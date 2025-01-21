2d4b3484e50470770f021dfd1d2ad50e8fc28a7e
Seungsoo Baek <bss.rol20@gmail.com>
2025-01-21 14:32:25 +0900

    안드로이드 플러터 웹뷰 관련 설정

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["lucide-react"], // add this
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [{ key: "Access-Control-Allow-Origin", value: "*" }],
+      },
+    ];
+  },
 };
 
 export default nextConfig;

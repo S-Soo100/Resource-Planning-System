9f636843fb5105e07a8f1f349e5d8aebf8b6ff45
Seungsoo Baek <bss.rol20@gmail.com>
2025-05-08 15:37:50 +0900

    feat: 품목 출고 리렌더링 최적화

--- src/types/(order)/orderRequestFormData.ts
+++ src/types/(order)/orderRequestFormData.ts
@@ -1,4 +1,7 @@
 import { TeamItem } from "../team-item";
+import { Warehouse } from "../warehouse";
+import { Item } from "../item";
+import { ApiResponse } from "../common";
 
 export type OrderRequestFormData = {
   supplierId?: number | null | undefined;
@@ -25,4 +28,7 @@ export type OrderItemWithDetails = {
 export interface OrderRequestFormProps {
   isPackageOrder?: boolean;
   title?: string;
+  warehousesList?: Warehouse[];
+  warehouseItems?: { [warehouseId: string]: Item[] };
+  onWarehouseChange?: (warehouseId: number) => Promise<ApiResponse>;
 }

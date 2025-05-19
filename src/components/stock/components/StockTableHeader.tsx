f2f05b200143a6f720696c5a8b41b884e4043c9f
Seungsoo Baek <bss.rol20@gmail.com>
2025-05-19 17:19:26 +0900

    feat: 아이템 검색 로진 변경, 모바일 UI 최적화

--- src/components/stock/components/StockTableHeader.tsx
+++ src/components/stock/components/StockTableHeader.tsx
@@ -56,11 +56,13 @@ export default function StockTableHeader({
       </div>
 
       <div className="mb-8">
-        <div className="flex justify-between items-center mb-4 px-4">
-          <h2 className="text-xl font-bold">{warehouseName}</h2>
+        <div className="flex flex-col justify-between items-center mb-4 px-4 gap-2">
+          <h2 className="flex justify-start text-start w-full text-xl font-bold">
+            {warehouseName}
+          </h2>
 
           {showButtons && (
-            <div className="flex items-center space-x-2">
+            <div className="flex items-center space-x-2 justify-end w-full ">
               <button
                 onClick={onInboundClick}
                 className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg flex items-center transition-colors duration-200"

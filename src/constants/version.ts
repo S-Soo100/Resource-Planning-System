02686dfdfa858bf5cfbb8a94508fdd327a32024d
Seungsoo Baek <bss.rol20@gmail.com>
2025-06-20 14:17:23 +0900

    1.4.0 release

--- src/constants/version.ts
+++ src/constants/version.ts
@@ -1,3 +1,3 @@
-export const APP_VERSION = "1.3.0";
+export const APP_VERSION = "1.4.0";
 export const APP_NAME = "KARS 재고관리 시스템";
 export const COPYRIGHT = "© 2025 Kangsters. All rights reserved.";
